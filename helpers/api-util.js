7e44f9f74f2f2202fb17060f0f00284d45b44f39
yuuka-wtnb <yuuka.wtnb.011@gmail.com>
2023-07-26 03:59:27 +0900

    125 Using Server-side Rendering (SSR)

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -12,7 +12,7 @@ export async function getAllEvents() {
     events.push({
       id: key,
       //spread operator and just copy everything from data for the given key
-      ...data[key]
+      ...data[key],
     });
   }
 
@@ -25,6 +25,21 @@ export async function getFeaturedEvents() {
 }
 
 export async function getEventById(id) {
-    const allEvents = await getAllEvents();
-    return allEvents.find((event) => event.id === id);
-  }
+  const allEvents = await getAllEvents();
+  return allEvents.find((event) => event.id === id);
+}
+
+export async function getFilteredEvents(dateFilter) {
+  const { year, month } = dateFilter;
+
+  const allEvents = await getAllEvents();
+
+  let filteredEvents = allEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredEvents;
+}
